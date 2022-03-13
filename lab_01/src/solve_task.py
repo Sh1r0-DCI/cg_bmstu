0f62bc405d48408b32118847a10dc44fa3912495
NeSh1r0 <khamit-7@yandex.ru>
2022-03-13 17:52:58 +0300

    already sth

--- lab_01/src/solve_task.py
+++ lab_01/src/solve_task.py
@@ -20,12 +20,14 @@ def form_coord_matrix(set_points):
 def is_n_gon(polygon):
     comb = combinations(polygon, 3)
     for i in comb:
-        triangle = Triangle(Point(i[0][0], i[0][1]),
-                            Point(i[1][0], i[1][1]),
-                            Point(i[2][0], i[2][1]))
-        print('current triangle:', triangle)
-        if triangle is not Triangle:
-            print('is not triangle')
+        if i[2][0] == i[0][0] == i[1][0] or \
+                i[2][1] == i[0][1] == i[1][1]:
+            return False
+        elif i[1][0] - i[0][0] == 0 or \
+                i[1][1] - i[0][1] == 0:
+            continue
+        elif (i[2][0] - i[0][0]) / (i[1][0] - i[0][0]) == \
+                (i[2][1] - i[0][1]) / (i[1][1] - i[0][1]):  # check if 3 points lie on the same line
             return False
     return True
 
@@ -33,122 +35,40 @@ def is_n_gon(polygon):
 def form_polygons_list(polygons, coord_set, n_vertices):
     comb = combinations(coord_set, n_vertices)
     for i in comb:
-        print('current polygon:', i)
         if is_n_gon(i):
             polygons.append(i)
 
 
 def gauss_area_algorithm(polygon, n_vertices):  # trapezoid formula
-    trapezoid_sum = 0
+    # trapezoid_sum = 0
+    # for i in range(0, n_vertices - 1):
+    #     trapezoid_sum += abs((polygon[i][0] - polygon[i+1][0]) * (polygon[i][1] + polygon[i+1][1]))
+    # return trapezoid_sum / 2
+    pos_sum = 0
+    neg_sum = 0
     for i in range(0, n_vertices - 1):
-        trapezoid_sum += (polygon[i][0] - polygon[i+1][0]) * (polygon[i][1] + polygon[i+1][1])
-    return trapezoid_sum / 2
+        pos_sum += polygon[i][0] * polygon[i+1][1]
+        neg_sum += polygon[i+1][0] * polygon[i][1]
+    return 0.5 * abs(pos_sum + polygon[n_vertices - 1][0] * polygon[0][1] -
+                     neg_sum - polygon[0][0] * polygon[n_vertices - 1][1])
+
 
 
 def find_desired_polygon(desired_polygon, polygons, n_vertices):
     max_area = 0
     for polygon in polygons:
         cur_area = gauss_area_algorithm(polygon, n_vertices)
+        print('current polygon:', polygon)
+        print('its area:', cur_area)
         if cur_area > max_area:
             max_area = cur_area
+            print('new poligon arrived:', polygon)
             desired_polygon = deepcopy(polygon)
+    return max_area
 
 
 def section_len(sx, sy, ex, ey):
     return sqrt((sx - ex)**2 + (sy - ey)**2)
-
-
-def get_zoom_vars(circles):
-    c1x_min = circles[0][0][0] - circles[0][0][2]
-    c2x_min = circles[1][0][0] - circles[1][0][2]
-
-    c1x_max = circles[0][0][0] + circles[0][0][2]
-    c2x_max = circles[1][0][0] + circles[1][0][2]
-
-    c1y_min = circles[0][0][1] - circles[0][0][2]
-    c2y_min = circles[1][0][1] - circles[1][0][2]
-
-    c1y_max = circles[0][0][1] + circles[0][0][2]
-    c2y_max = circles[1][0][1] + circles[1][0][2]
-
-    x1 = circles[0][0][0]
-    y1 = circles[0][0][1]
-    x2 = circles[1][0][0]
-    y2 = circles[1][0][1]
-
-    k_l = (y2 - y1) / (x2 - x1)
-    b = y1 - k_l * x1
-
-    min_x = min(c1x_min, c2x_min, b)
-    min_y = min(c1y_min, c2y_min, b)
-    max_x = max(c1x_max, c2x_max, b)
-    max_y = max(c1y_max, c2y_max, b)
-
-    k_z = canvas_width / max(abs(max_x - min_x), abs(max_y - min_y))
-
-    return [k_z, [min_x, min_y], [max_x, max_y]]
-
-
-def zoom(circles, zoom_vars):
-    k_z = zoom_vars[0]
-    min_x = zoom_vars[1][0]
-    min_y = zoom_vars[1][1]
-
-    for i in range(len(circles) - 1):
-        circles[i][0][2] *= k_z
-        for j in range(len(circles[i])):
-            circles[i][j][0] = k_z * (circles[i][j][0] - min_x)
-            circles[i][j][1] = k_z * (circles[i][j][1] - min_y)
-
-
-def get_h_of_triangle(a, b, c):                             # a = [0], b = [1], c = [2]
-    # s = abs((a.x-c.x)*(b.y-c.y)-(b.x-c.x)*(a.y-c.y))/2
-    ab = section_len(a[0], a[1], b[0], b[1])
-    bc = section_len(b[0], b[1], c[0], c[1])
-    ac = section_len(a[0], a[1], c[0], c[1])
-
-    # use of heron formula for finding max altitude
-    semi_perimeter = (ab + bc + ac)/2
-    current_area = sqrt(semi_perimeter*(semi_perimeter - ab) *
-                        (semi_perimeter - bc)*(semi_perimeter-ac))
-
-    ab_h = 2 * current_area / ab
-    ab_coor_x = ((b[0] - a[0]) * (b[1] - a[1]) * (c[1] - a[1]) + a[0] * ((b[1] - a[1])**2) +
-                 c[0] * ((b[0] - a[0])**2)) / (((b[1] - a[1])**2) + ((b[0] - a[0])**2))
-    if b[0] == a[0]:
-        ab_coor_y = c[1]
-    else:
-        ab_coor_y = (b[1] - a[1]) * (ab_coor_x - a[0]) / (b[0] - a[0]) + a[1]
-
-
-    bc_h = 2 * current_area / bc
-    bc_coor_x = ((c[0] - b[0]) * (c[1] - b[1]) * (a[1] - b[1]) + b[0] * ((c[1] - b[1])**2) +
-                 a[0] * ((c[0] - b[0])**2)) / (((c[1] - b[1])**2) + ((c[0] - b[0])**2))
-    if c[0] == b[0]:
-        bc_coor_y = a[1]
-    else:
-        bc_coor_y = (c[1] - b[1]) * (bc_coor_x - b[0]) / (c[0] - b[0]) + b[1]
-
-
-    ac_h = 2 * current_area / ac
-    ac_coor_x = ((c[0] - a[0]) * (c[1] - a[1]) * (b[1] - a[1]) + a[0] * ((c[1] - a[1])**2) +
-                 b[0] * ((c[0] - a[0])**2)) / (((c[1] - a[1])**2) + ((c[0] - a[0])**2))
-    if c[0] == a[0]:
-        ac_coor_y = b[1]
-    else:
-        ac_coor_y = (c[1] - a[1]) * (ac_coor_x - a[0]) / (c[0] - a[0]) + a[1]
-
-
-    if ab_h > bc_h:
-        if ab_h > ac_h:
-            return ab_h, ab_coor_x, ab_coor_y, "ab"
-        else:
-            return ac_h, ac_coor_x, ac_coor_y, "ac"
-    else:
-        if bc_h > ac_h:
-            return bc_h, bc_coor_x, bc_coor_y, "bc"
-        else:
-            return ac_h, ac_coor_x, ac_coor_y, "ac"
 
 
 def get_vertex(side_name, triangle):
@@ -159,11 +79,13 @@ def get_vertex(side_name, triangle):
     if side_name == 'bc':
         return triangle[0]
 
+
 def get_alt_vertex(side_name, triangle):
     if side_name == 'ab' or side_name == 'ac':
         return triangle[0]
     else:
         return triangle[1]
+
 
 def find_odds_for_drawing(desired_triangle, max_x, max_y):
     x_min = min(desired_triangle[0][0], desired_triangle[1][0], desired_triangle[2][0], max_x)
@@ -223,10 +145,8 @@ def draw_all_stuff(canvas, desired_triangle, desired_vertex, dash_vertex, max_x,
 
 
 def solve_task(canvas, listbox_set, n_vertices):
-    # coord_set = list(listbox_set.get(0, END))
-    # form_coord_matrix(coord_set)
-    print((Triangle(Point(0, 0), Point(1, 1), Point(3, 0))) is Triangle)
-    coord_set = ([0, 0], [1, 1], [2, 2], [3, 0])
+    coord_set = list(listbox_set.get(0, END))
+    form_coord_matrix(coord_set)
 
     polygons = []
     form_polygons_list(polygons, coord_set, n_vertices)
@@ -237,30 +157,22 @@ def solve_task(canvas, listbox_set, n_vertices):
         return
 
     desired_polygon = []
+    area_of_dp = find_desired_polygon(desired_polygon, polygons, n_vertices)
 
-    for triangle in triangles:
-        cur_h, cur_x, cur_y, cur_side = get_h_of_triangle(triangle[0], triangle[1], triangle[2])
-        if cur_h > max_h:
-            desired_triangle = deepcopy(triangle)
-            alt_side = cur_side
-            max_h = cur_h
-            max_x = cur_x
-            max_y = cur_y
-        
-    desired_vertex = get_vertex(alt_side, desired_triangle)
-    dash_vertex = get_alt_vertex(alt_side, desired_triangle)
+    print(desired_polygon)
+    print(area_of_dp)
 
 
-    showinfo(
-        "Высота",
-        "Была получена высота длиной {:.1f} , опущенная на сторону {}".format(max_h, alt_side),
-    )
+    # showinfo(
+    #     "Высота",
+    #     "Была получена высота длиной {:.1f} , опущенная на сторону {}".format(max_h, alt_side),
+    # )
 
     canvas.delete("all")
 
-    kx, ky, x_min, y_min, x_indent, y_indent = find_odds_for_drawing(desired_triangle, max_x, max_y)
-    draw_all_stuff(canvas,
-                   desired_triangle, desired_vertex, dash_vertex,
-                   max_x, max_y,
-                   kx, ky,
-                   x_indent, y_indent)
+    # kx, ky, x_min, y_min, x_indent, y_indent = find_odds_for_drawing(desired_triangle, max_x, max_y)
+    # draw_all_stuff(canvas,
+    #                desired_triangle, desired_vertex, dash_vertex,
+    #                max_x, max_y,
+    #                kx, ky,
+    #                x_indent, y_indent)
